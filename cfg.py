1229be257a8983390740734a17949dded85f097f
Eric <xiaoxiang.cn@gmail.com>
2018-05-16 16:11:59 +0800

    Support multiple svn.

--- cfg.py
+++ cfg.py
@@ -1,20 +1,10 @@
 config = {
-    'svn':  'svn://xxxxxxx/%s/trunk/',
-    'user': 'xxx',
-    'pass': 'xxx',
-    'poll': 5,
-    'cmds': ['pip install pytest; pytest'],
+    'svn': '',
+    'user': '',
+    'pass': '',
+    'poll': 120,
+    'cmds': [''],
     'srcs': [
-        "user",
     ],
-    'mail': {
-        'fromaddr': "x@y",
-        'sendToInterestedUsers': False,
-        'extraRecipients': ["x@y"],
-        'relayhost': "smtp.163.com",
-        'smtpPort': 25,
-        'smtpUser': "xxx",
-        'smtpPassword': "xxx",
-    },
-    'web': "http://localhost:8000/",
+    'web': "localhost:8000/",
 }
